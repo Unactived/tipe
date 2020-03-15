5217f5526626c42f98d2233c1a82e87288286ce3
Daleko <35976489+BardzoDaleko@users.noreply.github.com>
2020-03-15 15:02:55 +0100

    Update creationnoeud.py

--- creationnoeud.py
+++ creationnoeud.py
@@ -3,10 +3,28 @@
 #min := nombre minimal d'élément dans les feuilles (à partir de 1)
 #liste_caract := liste des caractéristiques sauf la var_cible
 #groupe := lsite de dictionnaires (représentant les éléments)
-
-import fonctions_d'heretogeneite
-
+resultats_fonction_heterogeneite = []
 for caracteristique in liste_caract:
     #les dictionnaires de la liste sont classés par ordre croissant selon la caractéristique
-    l = sorted(groupe, key=lambda variable: variable[caracteristique])
-    a= [procedure_indice_de_diversite_de_Gini(groupe, var_cible_pos, caracteristique, indice_de_decoupage) for indice_de_decoupage in range(min, len(groupe)-min) ]
+    groupe = sorted(groupe, key=lambda variable: variable[caracteristique])
+    resultats_fonction_heterogeneite.append([procedure_indice_de_diversite_de_Gini(groupe, var_cible_pos, caracteristique, indice_de_decoupage) for indice_de_decoupage in range(min, len(groupe)-min) ] )
+
+min_indice_1 = 0
+l_min_indice_2 = []
+for indice_1 in range(len(resultats_fonction_heterogeneite)):
+    min_indice_2 = 0
+    min_var_2 = resultats_fonction_heterogeneite[indice_1][0]
+    for indice_2, var_2 in enumerate(resultats_fonction_heterogeneite[indice_1]):
+        if var_2 < min_var_2:
+            min_indice_2 = indice_2
+            min_var_2 = var_2
+    l_min_indice_2.append([min_indice_2, min_var_2 ])
+
+min_var = l_min_indice_2[0][1]
+min_indice = 0
+for i in range(len(l_min_indice_2)):
+    if l_min_indice_2[i][1] < min_var:
+        min_indice = i
+        min_var = l_min_indice_2[i][1]
+
+print("variable à étudier :", liste_caract[min_indice], "\n valeur seuil :", l_min_indice_2[min_indice][1])
