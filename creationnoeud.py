97608d365c9670678920e6a688ebca615fc42eec
Daleko <35976489+BardzoDaleko@users.noreply.github.com>
2020-03-21 09:51:11 +0100

    Changement min en max

--- creationnoeud.py
+++ creationnoeud.py
@@ -8,7 +8,7 @@ def creation_noeud(var_cible:str, var_cible_pos:list, min:int, liste_caract:list
     resultats_fonction_heterogeneite = []
     for caracteristique in liste_caract:
         groupe = sorted(groupe, key=lambda variable: variable[caracteristique])#les dictionnaires de la liste sont classés par ordre croissant selon la caractéristique
-        resultats_fonction_heterogeneite.append([procedure_indice_de_diversite_de_Gini(groupe, var_cible_pos, caracteristique, indice_de_decoupage) for indice_de_decoupage in range(min, len(groupe)-min) ] )
+        resultats_fonction_heterogeneite.append( [ procedure_indice_de_diversite_de_Gini(groupe, var_cible_pos, caracteristique, indice_de_decoupage) for indice_de_decoupage in range(min, len(groupe)-min) ] )
         #la liste créée par compréhension est la liste des valeurs renvoyées par la fonction d'hétérogénéité pour toute les valeurs possibles
         #Ainsi, dans resultats_fonction_heterogeneite, il y a une liste par caractéristiques
     
@@ -17,25 +17,25 @@ def creation_noeud(var_cible:str, var_cible_pos:list, min:int, liste_caract:list
     #Ainsi, on a le meilleur point de coupure par caractéristique.
     #Ensuite, on compare le minimum des caractéristiques. On aboutit au minimum parmi toutes les caractéristiques et toutes les valeurs, càd le meilleur point de coupure.
     #On travaille sur les indices, pour garder en mémoire l'indice du minimum, puisque l'on veut à la fin savoir quel point de coupure garder, puis quelle variable étudier.
-    min_indice_1 = 0
-    l_min_indice_2 = []
+    max_indice_1 = 0
+    l_max_indice_2 = []
     for indice_1 in range(len(resultats_fonction_heterogeneite)):#on cherche l'indice du minimum et le minimum de chaque sous-liste de resultats_fonction_heterogeneite
-        min_indice_2 = 0
-        min_var_2 = resultats_fonction_heterogeneite[indice_1][0]
-        for indice_2, var_2 in enumerate(resultats_fonction_heterogeneite[indice_1]):
-            if var_2 < min_var_2:
-                min_indice_2 = indice_2
-                min_var_2 = var_2
-        l_min_indice_2.append([min_indice_2, min_var_2 ])#on obtient un liste de sous-liste
-        #Dans l_min_indice_2, chaque sous-liste correspond à une caractéristique.
+        max_indice_2 = 0
+        max_var_2 = resultats_fonction_heterogeneite[indice_1][0]
+        for indice_2, var_2 in enumerate( resultats_fonction_heterogeneite[indice_1] ):
+            if var_2 > max_var_2:
+                max_indice_2 = indice_2
+                max_var_2 = var_2
+        l_max_2.append( [ max_indice_2, max_var_2 ] )#on obtient un liste de sous-listes
+        #Dans l_max_2, chaque sous-liste correspond à une caractéristique.
         #Une sous-liste comprend l'indice et la valeur du minimum des renvoies de la fonction d'hétérogénéité pour une caractéristiques
 
     #on cherche à présent le minimum entre les sous-listes
-    min_var = l_min_indice_2[0][1]
-    min_indice = 0
-    for i in range(len(l_min_indice_2)):
-        if l_min_indice_2[i][1] < min_var:
-            min_indice = i
-            min_var = l_min_indice_2[i][1]
+    max_var = l_max_2[0][1]
+    max_indice = 0
+    for i in range(len(l_max_indice_2)):
+        if l_max_2[i][1] > max_var:
+            max_indice = i
+            max_var = l_max_2[i][1]
 
-    print("variable à étudier :", liste_caract[min_indice], "\n valeur seuil :", l_min_indice_2[min_indice][1])
+    return  liste_caract[max_indice], l_max_2[min_indice][1] # variable à étudier, valeur seuil
