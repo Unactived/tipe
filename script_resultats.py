376d8fae14cbb90360550c4482ffa82fb66b779f
Daleko <35976489+BardzoDaleko@users.noreply.github.com>
2020-06-03 11:23:38 +0200

    ajout comptage de feuille ; augmentation de la limite de récursion

--- script_resultats.py
+++ script_resultats.py
@@ -3,6 +3,9 @@ from CHAID import *
 from pickle import load
 from pickle import dump
 
+import sys
+sys.setrecursionlimit(10000)
+
 with open("arbre_cart.pickle", 'rb') as fichier:
     arbre_cart,jeu_apprentissage, jeu_test, t_cart = load(fichier)
 
@@ -13,8 +16,12 @@ with open("arbre_chaid.pickle", 'rb') as fichier:
     arbre_chaid,jeu_apprentissage, jeu_test, t_chaid = load(fichier)
 
 var_cible, var_cible_pos = "final", [0,1]
-print("TAUX ERREUR AVEC LE JEU DE TEST")
-print("arbre_cart :", taux_erreur_test_arbre(arbre_cart, jeu_test, var_cible, var_cible_pos))
+print("compter_feuille(arbre_cart)", compter_feuille(arbre_cart))
+print("compter_feuille(arbre_elage)", compter_feuille(arbre_elage))
+#arbre_cart.afficher()
+#arbre_elage.afficher()
+print("\nTAUX ERREUR AVEC LE JEU DE TEST")
+print("arbre :", taux_erreur_test_arbre(arbre_cart, jeu_test, var_cible, var_cible_pos))
 print("arbre_elage :", taux_erreur_test_arbre(arbre_elage, jeu_test, var_cible, var_cible_pos))
 print("arbre_chaid :", taux_erreur_test_arbre(arbre_chaid, jeu_test, var_cible, var_cible_pos))
 
