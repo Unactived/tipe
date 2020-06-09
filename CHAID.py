7fccee4fcc8cec343400a8131fbbdc990514d8ab
Daleko <35976489+BardzoDaleko@users.noreply.github.com>
2020-06-09 23:13:28 +0200

    correction

--- CHAID.py
+++ CHAID.py
@@ -5,20 +5,41 @@ def test_khi_2(groupe:list, var_cible:str, var_cible_pos:tuple, indice_de_decoup
     groupeGauche, groupeDroite = groupe[indice_de_decoupage:], groupe[:indice_de_decoupage]
 
     khi_2 = 0
-
+    
+    l_khi2 = []
     for pos in var_cible_pos:
 
         G = sum([1 for groupe in groupeGauche if groupe[var_cible] == pos])
         D = sum([1 for groupe in groupeDroite if groupe[var_cible] == pos])
 
-        if G == D == 0:
-            continue
+        l_khi2.append(G)
+        l_khi2.append(D)
 
-        theorique = (G + D) / 2
+    total = len(groupe)
+    total_G = len(groupeGauche)
+    total_D = len(groupeDroite)
+    total_non_spam = l_khi2[0] + l_khi2[1]
+    total_spam = l_khi2[2] + l_khi2[3]
 
-        khi_2 += (G - theorique)**2 / theorique
-        khi_2 += (D - theorique)**2 / theorique
+    G_non_spam = l_khi2[0]
+    D_non_spam = l_khi2[1]
+    G_spam = l_khi2[2]
+    D_spam = l_khi2[3]
 
-    if khi_2 < 3.84:
+    theorique_G_non_spam = total_non_spam*total_G/total
+    theorique_D_non_spam = total_non_spam*total_D/total
+    theorique_G_spam = total_spam*total_G/total
+    theorique_D_spam = total_spam*total_D/total
+
+    if theorique_G_non_spam != 0:
+        khi_2 += (G_non_spam - theorique_G_non_spam)**2 / theorique_G_non_spam
+    if theorique_D_non_spam != 0:
+        khi_2 += (D_non_spam - theorique_D_non_spam)**2 / theorique_D_non_spam
+    if theorique_G_spam != 0:
+        khi_2 += (G_spam - theorique_G_spam)**2 / theorique_G_spam
+    if theorique_D_spam != 0:
+        khi_2 += (D_spam - theorique_D_spam)**2 / theorique_D_spam
+
+    if khi_2 < 3.841:
         return 0
     return khi_2
